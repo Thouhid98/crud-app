8390e45d6b0a3da46443cd1e9ead4b66f3868909
Thouhid98 <thouhidhussain98@gmail.com>
2024-05-07 16:46:54 +0600

    Edit functionality added

--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
+"use client";
 
-const EditTopicForm = ({id}) => {
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-    const removeTopic = async () => {
+export default function EditTopicForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
 
-        const res = await fetch(`http://localhost:3000/api/topics?id=${id}`)
-            
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div>
-            <form className='flex flex-col gap-3'>
-                <input type="text" placeholder='Topic Title' className='border border-slate-400 px-8 py-3' />
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
 
-                <input type="text" placeholder='Topic Description' className='border border-slate-400 px-8 py-3' />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
 
-                <button className='font-bold text-white bg-green-800 px-6 py-3 w-fit'>Update Topic</button>
-            </form>
-        </div>
-    );
-};
-
-export default EditTopicForm; <h2>Edit form</h2>
+      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        Update Topic
+      </button>
+    </form>
+  );
+}
